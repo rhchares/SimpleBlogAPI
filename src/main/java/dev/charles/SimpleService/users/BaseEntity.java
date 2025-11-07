4d944540a0ba57fc9e66fbed78570cf3b09d8308
Charles <rhcharles0@gmail.com>
2025-11-07 21:32:05 +0900

    feat(entity): seperated common properities of a entity for auditing

---
+++ src/main/java/dev/charles/SimpleService/users/BaseEntity.java
@@ -0,0 +1,24 @@
+package dev.charles.SimpleService.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Instant updatedAt;
+
+}
