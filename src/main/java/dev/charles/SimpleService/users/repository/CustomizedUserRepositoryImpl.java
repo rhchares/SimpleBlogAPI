6935c018211cb87f9757e8b5b4f06b6bf3f3a86a
Charles <rhcharles0@gmail.com>
2025-11-09 13:58:46 +0900

    fix: fix error by moving.

--- src/main/java/dev/charles/SimpleService/users/repository/CustomizedUserRepositoryImpl.java
+++ src/main/java/dev/charles/SimpleService/users/repository/CustomizedUserRepositoryImpl.java
@@ -8,7 +8,8 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static dev.charles.SimpleService.users.QUsers.users;
+import static dev.charles.SimpleService.users.domain.QUsers.users;
+
 
 @RequiredArgsConstructor
 public class CustomizedUserRepositoryImpl implements CustomizedUserRepository{
