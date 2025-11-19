bc82a58e5ff2196e72c19de53e91a2ad8492a654
Charles <rhcharles0@gmail.com>
2025-11-19 20:41:02 +0900

    chore: delete a task of queryDsl

--- src/main/java/dev/charles/SimpleService/errors/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package dev.charles.SimpleService.errors.exception;
-
-import dev.charles.SimpleService.errors.errorcode.CustomErrorCode;
-
-public class AuthorizationException extends RestApiException {
-    public AuthorizationException(String message) {
-        super(CustomErrorCode.NOT_AUTHORIZED, message);
-    }
-}
